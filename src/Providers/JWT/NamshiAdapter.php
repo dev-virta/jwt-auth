17f8193e103a26e252ad9badfe639556e19fe3a6
Khoa Nguyen <khoa.nguyen@virta.global>
2019-05-25 14:08:40 +0300

    Fix the bug in verifying token using deprecated method

--- src/Providers/JWT/NamshiAdapter.php
+++ src/Providers/JWT/NamshiAdapter.php
@@ -55,7 +55,7 @@ class NamshiAdapter extends JWTProvider implements JWTInterface
             throw new TokenInvalidException('Could not decode token: ' . $e->getMessage());
         }
 
-        if (!$jws->verify($this->secret, $this->algo) && !$this->verifyJWSWithDeprecatedMethod($jws, $this->secret)) {
+        if (!$jws->verify($this->secret, $this->algo) && !$this->verifyJWSWithDeprecatedMethod($jws, $token, $this->secret)) {
             throw new TokenInvalidException('Token Signature could not be verified.');
         }
 
@@ -66,19 +66,39 @@ class NamshiAdapter extends JWTProvider implements JWTInterface
      * Verify the JWS token with the old method
      *
      * @param  JWS  $jws
+     * @param  string $token
      * @param  string $key
      * @return bool
      */
-    private function verifyJWSWithDeprecatedMethod($jws, $key)
+    private function verifyJWSWithDeprecatedMethod($jws, $token, $key)
     {
-        $encoder = $this->getEncoderFromToken($jws->getTokenString());
+        $encoder = $this->getEncoderFromToken($token);
         $decodedSignature = $encoder->decode($jws->getEncodedSignature());
-        $signinInput = $jws->generateSigninInput();
+        $signinInput = $this->getSigninInput($jws, $token);
+        $test = $jws->getSigninInput();
 
         // Signed the input using deprecated method
         $signedInput = hash_hmac('sha256', $signinInput, (string) $key);
 
-        return hash_equals($signedInfo, $decodedSignature);
+        return hash_equals($signedInput, $decodedSignature);
+    }
+
+    /**
+     * Get the original token signin input if it exists, otherwise generate the
+     * signin input for the current JWS
+     *
+     * @param JWS $jws
+     * @return string
+     */
+    private function getSigninInput($jws, $token)
+    {
+        $parts = explode('.', $token);
+
+        if (count($parts) >= 2) {
+            return sprintf('%s.%s', $parts[0], $parts[1]);
+        }
+
+        return $jws->generateSigninInput();
     }
 
     /**
@@ -90,6 +110,6 @@ class NamshiAdapter extends JWTProvider implements JWTInterface
      */
     private function getEncoderFromToken($token)
     {
-        $encoder = strpbrk($token, '+/=') ? new Base64Encoder() : new Base64UrlSafeEncoder();
+        return strpbrk($token, '+/=') ? new Base64Encoder() : new Base64UrlSafeEncoder();
     }
 }
